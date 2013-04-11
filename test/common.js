03bfa2177738155db43cb9440911cfebe812aa9f
gustaff-weldon <juppie+github@gmail.com>
2013-04-11 16:45:44 +0200

    added setup for running tests on node

---
+++ test/common.js
@@ -0,0 +1,5 @@
+global.chai = require( "chai" );
+global.should = require( "chai" ).should();
+global.expect = require( "chai" ).expect;
+global.assert = require( "chai" ).assert;
+global.splash = require( "../lib/splash.js" );
